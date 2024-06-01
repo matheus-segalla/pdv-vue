b03c9e4079a22e3ede597c2b1843a96c55afec72
unknown <matheussegalla469@hotmail.com>
2024-06-01 13:12:07 -0300

    add product

--- src/views/PedidosView.vue
+++ src/views/PedidosView.vue
@@ -1,17 +1,12 @@
 <template>
     <div class="main-container">
       <h1>PEDIDOS</h1>
-      <PedidoForm/>
+      
     </div>
   </template>
   
   <script>
-  import PedidoForm from "../components/PedidoForm.vue"
 
-  export default {
-    components:{
-      PedidoForm,
-    }
-  }
+
   </script>
   
--- src/views/ProdutosView.vue
+++ src/views/ProdutosView.vue
@@ -1,20 +1,11 @@
 <template>
-<<<<<<< HEAD
     <div class="main-container">
       <h1>PRODUTOS</h1>
       <input ref="newproduct" placeholder="Novo Produto" type="text">
       <button @click="addNewProduct">Adicionar</button>
-=======
     <div>
         <div class="card">
-            <div class="card-header">
-                <h4>
-                    PRODUTOS
-                    <RouterLink to="/produto/create" class="btn btn-primary float-end"> 
-                        Adicionar Produto
-                    </RouterLink>
-                </h4>
-            </div>
+            
             <div class="card-body">
                 <table class="table table-bordered">
                     <thead>
@@ -31,19 +22,13 @@
                         </tr>
                     </tbody>
                 </table>
-            </div>
+            </div>s
         </div>
->>>>>>> d435f192abe55c458996b0923a6a6757f93cdfe0
     </div>
+</div>
   </template>
   
   <script>
-<<<<<<< HEAD
-  
-
-  </script>
-  
-=======
     import axios from 'axios';
     import { RouterLink } from 'vue-router';
 
@@ -72,6 +57,5 @@
   </script>
   
   <style>
-    @import 'bootstrap';
+   
   </style>
->>>>>>> d435f192abe55c458996b0923a6a6757f93cdfe0
